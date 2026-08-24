f987a5a1cfdf337e09a225379d788a203a79d26c
somba-kyalo <musombasamuel3@gmail.com>
2026-08-24 20:55:47 +0300

    project structure

---
+++ TailorApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TailorappConfig(AppConfig):
+    name = 'TailorApp'
--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vmf6dm8&av65r42ni-+khw-cc3_na%f^q2(_vbkuqi5khc6!jl'
+SECRET_KEY = 'django-insecure-yne)(9i#xb5mo1hfui3q@8!wu@ai=ks%20pnhk-uhcaz%$g(!g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AdminApp',
+    'AuthenticationApp',
+    'CustomerApp',
+    'JobApp',
+    'PaymentApp',
+    'ReviewApp',
+    'TailorApp',
+    'OrderApp',
+    'NotificationApp',
+    'MessagingApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static', ]

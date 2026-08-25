76bd46f75494df012b497141d3fd431c6db6d143
somba-kyalo <musombasamuel3@gmail.com>
2026-08-25 12:27:21 +0300

    tailorapp models

--- TailorApp/models.py
+++ TailorApp/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TailorProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tailor_profile'
+    )
+    business_name = models.CharField(max_length=150)
+    full_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=20)
+    location = models.CharField(max_length=150)
+    profile_picture = models.ImageField(
+        upload_to='tailor_profiles/',
+        blank=True,
+        null=True
+    )
+    bio = models.TextField(blank=True)
+    experience_years = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.business_name

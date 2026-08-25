c337355146e86f3513f3c0eb199b3003876420ac
somba-kyalo <musombasamuel3@gmail.com>
2026-08-25 12:40:10 +0300

    customer admin

--- CustomerApp/admin.py
+++ CustomerApp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomerProfile
+
+
+@admin.register(CustomerProfile)
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'user',
+        'phone',
+        'location',
+        'created_at',
+    )
+
+    search_fields = (
+        'full_name',
+        'phone',
+        'location',
+        'user__username',
+        'user__email',
+    )
+
+    list_filter = (
+        'location',
+        'created_at',
+    )

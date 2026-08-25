7871398b1bf0b4baf2dc6b1ef3d08f434084a73e
somba-kyalo <musombasamuel3@gmail.com>
2026-08-25 12:26:24 +0300

    customer models

--- CustomerApp/models.py
+++ CustomerApp/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='customer_profile'
+    )
+    full_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=20)
+    location = models.CharField(max_length=150)
+    profile_picture = models.ImageField(
+        upload_to='customer_profiles/',
+        blank=True,
+        null=True
+    )
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
